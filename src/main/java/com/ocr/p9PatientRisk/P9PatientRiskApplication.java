395d3126f0a5be5c42cbca70288d7cb1436b5f60
BARBERIS Olivier <olivier.barberis@axa.fr>
2021-03-02 18:23:40 +0100

    init

---
+++ src/main/java/com/ocr/p9PatientRisk/P9PatientRiskApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.p9PatientRisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P9PatientRiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P9PatientRiskApplication.class, args);
+    }
+
+}
